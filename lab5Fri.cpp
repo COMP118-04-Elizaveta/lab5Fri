5d18fc8172ec1c0d1f25dc190d7f58d3203e4df6
StylianosCoder <georgiou.s33@live.unic.ac.cy>
2025-05-16 10:39:49 +0300

    Add project files.

---
+++ lab5Fri.cpp
@@ -0,0 +1,21 @@
+/* \file lab5Fri.cpp
+* \brief This file contains
+* \author Elizaveta
+* \date 16 May 2025
+* \copyright University of Nicosia.
+*/
+
+#include <iostream>
+using namespace std;
+
+/**
+* function main
+* @return zero
+*/
+int main() {
+    cout << "Hello World!\n";
+
+    return 0;
+}
+
+
